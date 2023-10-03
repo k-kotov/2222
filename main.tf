8aaed498993d5cda6ecd7814922c5ac68966dcaf
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2023-10-03 21:11:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.86"
+      version = "3.0.0"
     }
   }
 }
@@ -150,4 +150,8 @@ resource "azurerm_virtual_machine_scale_set" "example" {
   tags = {
     environment = "staging"
   }
+}
+import {
+  to = null_resource.foo
+  id = ""
 }
