f48c3c6b8ca02430203922c9a1bf28fb3451508a
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2023-10-04 12:47:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,40 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.0"
+    required_providers {
+        scalr = {
+            source = "registry.scalr.dev/scalr/scalr"
+            version= "1.0.0-rc-develop"
+        }
     }
-  }
-}
-provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
 }
 
-resource "azurerm_storage_container" "example" {
-  name                  = "content"
-  storage_account_name  = "storageaccountnametest19"
-  container_access_type = "private"
-}
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "example<>resources"
-    storage_account_name = "storageaccountnametest19"
-    container_name       = "content"
-    key                  = "terraform.tfstate"
-  }
-}
+provider scalr {
+   hostname = "runs.kostya-runs.testenv.scalr.dev"
+   token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ1c2VyIiwianRpIjoiYXQtdjBvM2UxbGszZWUwYWsycGYifQ.h60fNZ1FB6IKtF5ptXmPidmbNCcIyWMIXQ4uckHTmXg"
+ }
+
+
+ 
+ 
+ resource "scalr_workspace" "more" {
+   count           = 5
+   name            = "my-workspace-name-${floor(count.index/5)}"
+  #environment_id  = scalr_environment.test[floor(count.index/2)].id
+  environment_id = "env-v0o3e1k06pqib57lv"
+  #draft-pr-runs-enabled = true
+   auto_apply = true
+   deletion_protection_enabled = false
+   #agent_pool_id = "apool-v0nv01vdeu2udhnho"
+   terraform_version = "latest"
+    vcs_provider_id = "vcs-v0o3e1k2d30u6e6jg"
+    vcs_repo {
+     identifier = "k-kotov/2222"
+      branch     = "main"
+      #dry_runs_enabled = true
+      #ingress_submodules = true
+
+      }
+    }
+
+
+    
